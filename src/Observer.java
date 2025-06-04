02f1e35eaa65d14c33d045dd87804d9417412e62
SGRuzo <sgarciaruzo@danielcastelao.org>
2025-06-04 13:17:38 +0200

    Observer

--- src/Observer.java
+++ src/Observer.java
@@ -2,4 +2,14 @@
 public class Observer {
     // límite mínimo de gasolina
     final static double LIMITE_GASOLINA = 10.0;
+
+    /**
+     * Metodo que se llama para actualizar el estado del coche
+     * @param coche el coche a actualizar
+     */
+    public static void update(Coche coche) {
+        if (coche.gasolina<LIMITE_GASOLINA){
+            View.alarmaReportar(coche.matricula);
+        }
+    }
 }
